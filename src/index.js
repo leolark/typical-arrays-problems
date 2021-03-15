1f7f55d343ac07f2b1a68214d50cd0f758338362
Leonte Zhavoronkov <32207706+leolark@users.noreply.github.com>
2021-03-15 23:17:10 +0300

    feat
    Update code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
-
 exports.min = function min (array) {
-  return 0;
+  if (array === undefined) return 0;
+  if (array.length === 0) return 0;
+  mymin = array[0];
+  for (i = 1; i < array.length; ++i) {
+     if (array[i] < mymin) mymin = array[i];
+  }
+  return mymin;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array === undefined) return 0;
+  if (array.length === 0) return 0;
+  mymax = array[0];
+  for (i = 1; i < array.length; ++i) {
+      if (array[i] > mymax) mymax = array[i];   
+  }
+  return mymax;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array === undefined) return 0;
+  if (array.length === 0) return 0;
+  let l = 0;
+  let summ = 0;
+  let midd = 0;
+  for (i = 0; i < array.length; i++) {
+      l = array.length;
+      summ += array[i];
+  }
+  midd = summ / l;
+  return midd;
 }
